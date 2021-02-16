fbfe592100417aab3d46a7b0db33758ca833e6ce
Johnatan Brayan <johnatanbrayan1@gmail.com>
2021-02-15 23:27:58 -0300

    Configure global styled and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
-  return <h1>Hello Word</h1>;
+  return (
+    <>
+      <h1>Hello Word</h1>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
