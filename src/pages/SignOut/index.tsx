e11feb87b2756fa0aafed2788842523a3eac82a7
Johnatan Brayan <johnatanbrayan1@gmail.com>
2021-02-17 23:55:46 -0300

    Getting data of input with useField of @unform

--- src/pages/SignOut/index.tsx
+++ src/pages/SignOut/index.tsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import { FiChevronLeft, FiUser, FiKey, FiMail } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './style';
 import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignOut: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logo} alt="Go Barber" />
+const SignOut: React.FC = () => {
+  function handleSubmit(data: unknown): void {
+    console.log(data);
+  }
 
-      <form action="">
-        <Input type="text" icon={FiUser} name="name" placeholder="Name" />
-        <Input type="text" icon={FiMail} name="email" placeholder="E-mail" />
-        <Input
-          type="password"
-          icon={FiKey}
-          name="password"
-          placeholder="Password"
-        />
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logo} alt="Go Barber" />
 
-        <Button type="submit">Register</Button>
-      </form>
+        <Form onSubmit={handleSubmit}>
+          <Input type="text" icon={FiUser} name="name" placeholder="Name" />
+          <Input type="text" icon={FiMail} name="email" placeholder="E-mail" />
+          <Input
+            type="password"
+            icon={FiKey}
+            name="password"
+            placeholder="Password"
+          />
 
-      <a href="/">
-        <FiChevronLeft />
-        Back to Login
-      </a>
-    </Content>
-  </Container>
-);
+          <Button type="submit">Register</Button>
+        </Form>
+
+        <a href="/">
+          <FiChevronLeft />
+          Back to Login
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignOut;
